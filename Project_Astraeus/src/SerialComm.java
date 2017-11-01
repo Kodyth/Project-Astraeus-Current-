8d8cda301a6eeddcee647ce5d1d47056d106205a
bwiz77 <bwiz77@me.com>
2017-11-01 16:28:11 -0400

    Test

--- Project_Astraeus/src/SerialComm.java
+++ Project_Astraeus/src/SerialComm.java
@@ -6,7 +6,7 @@ import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
 import java.util.Enumeration;
-
+//testing for git
 /**
  * SerialComm.java
  * Project Astraeus
