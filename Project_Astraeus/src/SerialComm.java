1076f356d0dafe5b58ebf5b821e6149663af4890
MountainMan2 <mountg@my.erau.edu>
2017-11-08 15:22:42 -0500

    Geoffrey Test

--- Project_Astraeus/src/SerialComm.java
+++ Project_Astraeus/src/SerialComm.java
@@ -45,7 +45,7 @@ public class SerialComm implements SerialPortEventListener {
 	public void initialize() {
                 // the next line is for Raspberry Pi and 
                 // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
-                System.setProperty("gnu.io.rxtx.SerialPorts", "COM9");
+                System.setProperty("gnu.io.rxtx.SerialPorts", "COM4");
 
 		CommPortIdentifier portId = null;
 		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
