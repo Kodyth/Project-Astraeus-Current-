3d6b2057e49765e87f4e656d5f442afc82de2af0
MountainMan2 <mountg@my.erau.edu>
2017-11-12 18:37:09 -0500

    Random bar lengths, and I "resolved" the off-by-one errors for until we get the arraylist working.

--- Project_Astraeus/src/BarLengthForData.java
+++ Project_Astraeus/src/BarLengthForData.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class BarLengthForData {
 	public double DataToBarLength(String Datatype, int barLength){
 		int locationOfinfo=0;
+		if(DataLog.allData.size()>0){
 		for(int i =0;i<(DataLog.allData.get(DataLog.allData.size()-1).size()-1);i++){
 			if (DataLog.allData.get(DataLog.allData.size()-1).get(i).getType() == Datatype){
 				locationOfinfo=i;
@@ -12,6 +13,7 @@ public class BarLengthForData {
 		}
 		
 		double rawlength = DataLog.allData.get(DataLog.allData.size()-1).get(locationOfinfo).getValue();
-		return((rawlength/barLength)*100);
+		return((rawlength/barLength)*100);}
+		else return Math.random()*100;
 	}
 }
