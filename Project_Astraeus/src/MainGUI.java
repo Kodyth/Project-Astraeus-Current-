c5ca07d3a0f7c8cbc5a08e635eb2b3a017afbefa
Kody Miller <millek36@my.erau.edu>
2017-11-12 21:28:07 -0500

    Current Version with Updates

--- Project_Astraeus/src/MainGUI.java
+++ Project_Astraeus/src/MainGUI.java
@@ -24,158 +24,149 @@ import javafx.geometry.Pos;
  * Will display location of satellite, temperature readings, and voltage readings.
  * Functions: Main, Start
  * 
- * FRANCE IS BACON
- * HAIL BRITTANIA
- * USA USA USA
  */
 public class MainGUI extends Application{
- private command_line command;
- private boolean trigger;
+	private command_line command;
+	private boolean trigger;
 
-    public static void main(String[] args) {
-    	try {
+	public static void main(String[] args) {
+		try {
 			SerialComm.Run();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-        launch(args);
-        
-        
-    }
+		launch(args);
 
-    	@Override
-    public void start(Stage primaryStage) {
-    		//Setting parameters for the Stage
-            
-    		primaryStage.setTitle("Astraeus");
-            primaryStage.setWidth(850);
-            primaryStage.setHeight(700);
-            
-            
-        //Creating Horizontal Box and Vertical Boxes that make up the main scene
-            HBox mainLayout = new HBox();
-            VBox vLeft = new VBox(30);
-            vLeft.setMinWidth(284);
-            vLeft.setPadding(new Insets(15, 0, 15, 0));
-            VBox vRight = new VBox(30);
-            vRight.setMinWidth(284);
-            VBox vCenter = new VBox (40);
-            vCenter.setMinWidth(282);
-            mainLayout.getChildren().addAll(vLeft, vCenter, vRight);
-            mainLayout.setStyle("-fx-background-color: WHITE");
-            
-        //Creating the image for use in the GUI
-            Image cube = new Image("Resources/Cubesat.png");
-            ImageView cubev = new ImageView(cube);
-            cubev.setFitWidth(274);
-            cubev.setFitHeight(354);
-            
-        // This is a temporary stand-in for the actual map function
-            Image dummyMap = new Image("Resources/dummymap.png");
-            ImageView map = new ImageView(dummyMap);
-            map.setFitWidth(274);
-            map.setFitHeight(285);
-            vLeft.getChildren().addAll(cubev, map);
-           
-        //Creating the main label for the GUI
-            BorderPane root = new BorderPane();
-            Label astraeus = new Label("Astraeus \n\n\n\n\n\n");
-            astraeus.setStyle("-fx-text-fill: BLACK; -fx-font: 50 century-gothic; -fx-font-weight: bold");
-            astraeus.setPadding(new Insets(0, 0, 10, 0));
-            root.setMaxWidth(Double.MAX_VALUE);
-            root.setCenter(astraeus);
-            
-        //Creating Buttons for main GUI
-            Button commandb = new Button("Control Cubesat/ View Commands");
-            commandb.setMaxWidth(Double.MAX_VALUE);
-            commandb.setMinHeight(50);
-            commandb.setOnAction(e -> {
-            	command_line cmd = new command_line();
-            	try {
-            	cmd.start(primaryStage);
-            	} catch (Exception e1) {
-            	e1.printStackTrace();
-            	}
-            });
-            
-            Button rtData= new Button("View Real-Time Data");
-            rtData.setMaxWidth(Double.MAX_VALUE);
-            rtData.setMinHeight(50);
-            Button rData = new Button("View Recorded Data");
-            rData.setMaxWidth(Double.MAX_VALUE);
 
-            rData.setMinHeight(50);
-            
-            /**************HELP***************/
-            Button tutorial = new Button("Tutorial");
-            tutorial.setMaxWidth(Double.MAX_VALUE);
-            tutorial.setMinHeight(50);
-            
-            tutorial.setOnAction(e -> {  	
-            	Tutorial t = new Tutorial();
-            	try {
-            	t.start(primaryStage);
-            	} catch (Exception e1) {
-            	e1.printStackTrace();
-            	}
-            });
-                       
-            vCenter.getChildren().addAll(root, commandb, rtData, rData, tutorial);
+	}
 
-            rData.setMinHeight(50);
-            Button help = new Button("Control Cubesat/ View Commands");
-            help.setMaxWidth(Double.MAX_VALUE);
-            help.setMinHeight(50);
-            vCenter.getChildren().addAll(root, commandb, rData, help);
+	@Override
+	public void start(Stage primaryStage) {
+		//Setting parameters for the Stage
 
-            
-            //creating bars for temp and voltage. arbitrary values inputed temporarily
-            BarLengthForData bar1 = new BarLengthForData();
-            BarLengthForData bar2 = new BarLengthForData();
-            
-           
-             Label tempe = new Label();
-             tempe.setStyle("-fx-text-fill: BLACK; -fx-font: 15 century-gothic");
-             Label volta = new Label();
-             volta.setStyle("-fx-text-fill: BLACK; -fx-font: 15 century-gothic");
-             Rectangle temp = new Rectangle();
-             
-             temp.setHeight(50);
-             temp.setFill(Color.web("RED"));
-             Rectangle volt = new Rectangle();
-             
-             volt.setHeight(50);
-             volt.setFill(Color.web("RED"));
-             Task task = new Task<Void>() {
-            	  @Override
-            	  public Void call() throws Exception {
-            	    
-            	    while (true) {
-            	      
-            	      Platform.runLater(new Runnable() {
-            	        @Override
-            	        public void run() {
-            	          double temperature = bar1.DataToBarLength("TMP",1);
-            	          double voltage = bar2.DataToBarLength("VOL",1);
-            	          temp.setWidth(temperature/400);
-            	          volt.setWidth(voltage/5);
-            	          volta.setText("Voltage: " + (voltage/100) + "\n");
-            	          tempe.setText("Temperature: " + (temperature/100) + "\n");
-            	        }				
-            	      });
-            	      //recommitting
-            	      Thread.sleep(1000);
-            	    }
-            	  }
-            	};
-            	Thread th = new Thread(task);
-            	th.setDaemon(true);
-            	th.start();
-             vRight.getChildren().addAll(tempe,temp,volta,volt);
-             Scene mainScene = new Scene(mainLayout);
-             primaryStage.setScene(mainScene);
-             primaryStage.show();
+		primaryStage.setTitle("Astraeus");
+		primaryStage.setWidth(850);
+		primaryStage.setHeight(700);
 
-    	}
+
+		//Creating Horizontal Box and Vertical Boxes that make up the main scene
+		HBox mainLayout = new HBox();
+		VBox vLeft = new VBox(30);
+		vLeft.setMinWidth(284);
+		vLeft.setPadding(new Insets(15, 0, 15, 0));
+		VBox vRight = new VBox(30);
+		vRight.setMinWidth(284);
+		VBox vCenter = new VBox (40);
+		vCenter.setMinWidth(282);
+		mainLayout.getChildren().addAll(vLeft, vCenter, vRight);
+		mainLayout.setStyle("-fx-background-color: WHITE");
+
+		//Creating the image for use in the GUI
+		Image cube = new Image("Resources/Cubesat.png");
+		ImageView cubev = new ImageView(cube);
+		cubev.setFitWidth(274);
+		cubev.setFitHeight(354);
+
+		// This is a temporary stand-in for the actual map function
+		Image dummyMap = new Image("Resources/dummymap.png");
+		ImageView map = new ImageView(dummyMap);
+		map.setFitWidth(274);
+		map.setFitHeight(285);
+		vLeft.getChildren().addAll(cubev, map);
+
+		//Creating the main label for the GUI
+		BorderPane root = new BorderPane();
+		Label astraeus = new Label("Astraeus \n\n\n\n\n\n");
+		astraeus.setStyle("-fx-text-fill: BLACK; -fx-font: 50 century-gothic; -fx-font-weight: bold");
+		astraeus.setPadding(new Insets(0, 0, 10, 0));
+		root.setMaxWidth(Double.MAX_VALUE);
+		root.setCenter(astraeus);
+
+		//Creating Buttons for main GUI
+		Button commandb = new Button("Control Cubesat/ View Commands");
+		commandb.setMaxWidth(Double.MAX_VALUE);
+		commandb.setMinHeight(50);
+		commandb.setOnAction(e -> {
+			command_line cmd = new command_line();
+			try {
+				cmd.start(primaryStage);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+
+		Button rtData= new Button("View Real-Time Data");
+		rtData.setMaxWidth(Double.MAX_VALUE);
+		rtData.setMinHeight(50);
+		Button rData = new Button("View Recorded Data");
+		rData.setMaxWidth(Double.MAX_VALUE);
+
+		rData.setMinHeight(50);
+
+		/**************HELP***************/
+		Button tutorial = new Button("Tutorial");
+		tutorial.setMaxWidth(Double.MAX_VALUE);
+		tutorial.setMinHeight(50);
+
+		tutorial.setOnAction(e -> {  	
+			Tutorial t = new Tutorial();
+			try {
+				t.start(primaryStage);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+
+		vCenter.getChildren().addAll(root, commandb, rtData, rData, tutorial);
+
+		rData.setMinHeight(50);
+		//creating bars for temp and voltage. arbitrary values inputed temporarily
+		BarLengthForData bar1 = new BarLengthForData();
+		BarLengthForData bar2 = new BarLengthForData();
+
+
+		Label tempe = new Label();
+		tempe.setStyle("-fx-text-fill: BLACK; -fx-font: 15 century-gothic");
+		Label volta = new Label();
+		volta.setStyle("-fx-text-fill: BLACK; -fx-font: 15 century-gothic");
+		Rectangle temp = new Rectangle();
+
+		temp.setHeight(50);
+		temp.setFill(Color.web("RED"));
+		Rectangle volt = new Rectangle();
+
+		volt.setHeight(50);
+		volt.setFill(Color.web("RED"));
+		Task task = new Task<Void>() {
+			@Override
+			public Void call() throws Exception {
+
+				while (true) {
+
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							double temperature = bar1.DataToBarLength("TMP",1);
+							double voltage = bar2.DataToBarLength("VOL",1);
+							temp.setWidth(temperature/400);
+							volt.setWidth(voltage/5);
+							volta.setText("Voltage: " + (voltage/100) + "\n");
+							tempe.setText("Temperature: " + (temperature/100) + "\n");
+						}				
+					});
+					
+					Thread.sleep(1000);
+				}
+			}
+		};
+		Thread th = new Thread(task);
+		th.setDaemon(true);
+		th.start();
+		vRight.getChildren().addAll(tempe,temp,volta,volt);
+		Scene mainScene = new Scene(mainLayout);
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
+
+	}
 
 }
