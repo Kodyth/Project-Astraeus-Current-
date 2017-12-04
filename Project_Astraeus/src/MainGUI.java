777009620df0893de6162a6652cbd2456bcd6b93
Bwiz77 <Bwiz77@155.31.35.104>
2017-12-03 22:52:19 -0500

    asfdaj;ksldf

--- Project_Astraeus/src/MainGUI.java
+++ Project_Astraeus/src/MainGUI.java
@@ -20,6 +20,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
 
